722ff5cb513279f628b9681e6a9d728809a570ed
Khagendra Adhikari <khagendra.adhikari@itsutra.com>
2021-03-31 23:49:45 +0545

    'fix'

--- push.js
+++ push.js
@@ -1,10 +1,24 @@
-var push = require('web-push')
+var push = require("web-push");
 
-let vapidKeys={
-    publicKey: 'BLSxos0g_qbsjSbW-LoQE9Mp8cTatvMS3l3OxczYQRjqwebOMtMkyIraTFK6bTtbOX4jw7k78lFxMKM2MqN-Z0U',
-    privateKey: 'txr2bUEUmcUC_dRVxOa7f8Tg5gEKDt6d5LuBU1u4Spw'
-  }
+let vapidKeys = {
+  publicKey:
+    "BLSxos0g_qbsjSbW-LoQE9Mp8cTatvMS3l3OxczYQRjqwebOMtMkyIraTFK6bTtbOX4jw7k78lFxMKM2MqN-Z0U",
+  privateKey: "txr2bUEUmcUC_dRVxOa7f8Tg5gEKDt6d5LuBU1u4Spw",
+};
 
-  push.setVapidDetails('mainto:test@code.co.uk',vapidKeys.publicKey,vapidKeys.privateKey);
-
-  push.sendNotification(sub,'test message')
+push.setVapidDetails(
+  "mainto:test@code.co.uk",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+let sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/fx1-2MAnwPY:APA91bE-uWJWPDC3Rydw7Q2HFA5LJfqEnLluywvwSpvxkSvtIAP918BCUjieoQ6-Pc01DKlTYSX4hgI34TCjJlB4MCglp7MEaNd0g8UxiKpZFoz19xUNFuBhn1-C7H8pDFjebGhpSzlk",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BB2SyhmzXL9QIAm6gKoK0tc-G5OmmMwTsI1-TjTFg622GMCypEEmVtNZV5IcvwoarYsx2Rl0PMJ79LsHVSzm9A0",
+    auth: "ZyZowfj5VvVcUSKxqHiZ2Q",
+  },
+};
+push.sendNotification(sub, "test message");
--- sw.js
+++ sw.js
@@ -1,3 +1,62 @@
-self.addEventListener("push", () => {
-  self.registration.sendNotification("test message", {});
-});
+/* eslint-disable no-restricted-globals */
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("push", function (event) {
+    if (event.data) {
+      console.log("This push event has data: ", event.data.text());
+      const title = event.data.text();
+      const options = {
+        body: "TicketId",
+        icon:
+          "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+        badge:
+          "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+        silent: false,
+        actions: [
+          {
+            action: "coffee-action",
+            title: "Coffee",
+            icon: "/images/demos/action-1-128x128.png",
+          },
+          {
+            action: "doughnut-action",
+            title: "Doughnut",
+            icon: "/images/demos/action-2-128x128.png",
+          },
+        ],
+  
+        timestamp: Date.parse('01 Jan 2000 00:00:00')
+      };
+  
+      // eslint-disable-next-line no-restricted-globals
+      self.registration.showNotification(title, options);
+    } else {
+      console.log("This push event has no data.");
+    }
+  });
+  
+  self.addEventListener("notificationclick", function (event) {
+    if (!event.action) {
+      // Was a normal notification click
+      console.log("Notification Click.");
+      return;
+    }
+  
+    switch (event.action) {
+      case "coffee-action":
+        console.log("User ❤️️'s coffee.");
+        break;
+      case "doughnut-action":
+        console.log("User ❤️️'s doughnuts.");
+        break;
+      case "gramophone-action":
+        console.log("User ❤️️'s music.");
+        break;
+      case "atom-action":
+        console.log("User ❤️️'s science.");
+        break;
+      default:
+        console.log(`Unknown action clicked: '${event.action}'`);
+        break;
+    }
+  });
+  
