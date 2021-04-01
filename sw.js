2891e265da356cb1ceabf0b08228c0b029b3afba
Khagendra Adhikari <khagendra.adhikari@itsutra.com>
2021-04-01 23:07:54 +0545

    'consolek'

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ self.addEventListener("activate", function (event) {
 
 self.addEventListener("push", function (event) {
   if (event.data) {
-    console.log(data)
+    console.log(event.data)
     let data = event.data.json();
     let dts = Math.floor(Date.now());
     const title = data.title;
